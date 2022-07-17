db7c0e5870f6ecdc71b78c1f8a3ed866886ad5e4
Andreas Ruehl <3062585+aruehl@users.noreply.github.com>
2022-07-17 21:21:03 +0200

    version 0.2.0 state parameter verified

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='oauth4cli',
-    version='0.1.1',
+    version='0.2.0',
     description='The most straight forward way of using standard python libraries for a authorization code flow via Browser for the CLI.',
     url='https://github.com/aruehl/oauth4cli',
     author='Andreas Rühl',
