e6a8287e2b3de56efe81ebdec8dff750fab962ae
Andreas Ruehl <3062585+aruehl@users.noreply.github.com>
2022-07-27 20:10:53 +0200

    update

--- example.py
+++ example.py
@@ -1,9 +1,19 @@
 import json
-from oauth2tools import OAuth4CLI
+from oauth2tools import OAuthTools, OAuth4CLI
 
 if '__main__' == __name__:
     with open("./config.json", "r") as json_file:
         app_config = json.load(json_file)
-    o4c = OAuth4CLI(**app_config.get('oauth4cli'))
+
+    tools = OAuthTools(**app_config.get('oidc'))
+    auth_url = tools.authorization_url(redirect_uri="http://example.com/callback")
+    print("authorization url:")
+    print(auth_url)
+
+    post_data = tools.code_to_token_post_data("12345")
+    print("post data:")
+    print(post_data)
+
+    o4c = OAuth4CLI(**app_config.get('oidc'))
     response = o4c.login()
     print(response.get('access_token'))
