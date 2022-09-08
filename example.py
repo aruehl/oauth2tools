c218ef3d5783c5d4eee912641312e910c5ee5ce1
Andreas Ruehl <3062585+aruehl@users.noreply.github.com>
2022-09-08 21:33:13 +0200

    exception handling added

--- example.py
+++ example.py
@@ -2,18 +2,19 @@ import json
 from oauth2tools import OAuthTools, OAuth4CLI
 
 if '__main__' == __name__:
-    with open("./config.json", "r") as json_file:
-        app_config = json.load(json_file)
+    try:
+        with open("./config.json", "r") as json_file:
+            app_config = json.load(json_file)
 
-    tools = OAuthTools(**app_config.get('oidc'))
-    auth_url = tools.authorization_url(redirect_uri="http://example.com/callback")
-    print("authorization url:")
-    print(auth_url)
+        tools = OAuthTools(**app_config.get('oidc'))
+        auth_url = tools.authorization_url(redirect_uri="http://example.com/callback")
+        print(f"authorization url: {auth_url}")
 
-    post_data = tools.code_to_token_post_data("12345")
-    print("post data:")
-    print(post_data)
+        post_data = tools.code_to_token_post_data(code="12345")
+        print(f"post data: {post_data}")
 
-    o4c = OAuth4CLI(**app_config.get('oidc'))
-    response = o4c.login()
-    print(response.get('access_token'))
+        o4c = OAuth4CLI(**app_config.get('oidc'))
+        response = o4c.login()
+        print(f"access-token: {response.get('access_token')}")
+    except Exception as e:
+        print(e)
