de355e0b5c454fb1ed2b1b121eef33b4f34e499d
Andreas Ruehl <3062585+aruehl@users.noreply.github.com>
2022-11-08 18:09:38 +0100

    token_refresh method added

--- oauth2tools/oauth2tools.py
+++ oauth2tools/oauth2tools.py
@@ -1,14 +1,20 @@
+import jwt
+import logging
 import requests
+import time
 
-from . import tools
+from . import tools, jwt_helper
+
 
 class OAuthTools(object):
 
-    def __init__(self, well_known_url: str, client_id: str,
-                 client_secret: str=None,
-                 scope: str=None,
-                 pkce: str=None,
-                 oidc: bool=True):
+    def __init__(self,
+                 well_known_url: str,
+                 client_id: str,
+                 client_secret: str = None,
+                 scope: str = None,
+                 pkce: str = None,
+                 oidc: bool = True):
         """
         Initiate the OAuthTools.
         :param well_known_url:
@@ -28,12 +34,14 @@ class OAuthTools(object):
         self.state = None
         self.nonce = None
         self.code_verifier = None
+        self.refresh_token = None
 
     def _post_for_token(self, form_data: dict):
         response = requests.post(self.well_known.get('token_endpoint'), data=form_data)
         response.raise_for_status()
-
-        return response.json()
+        response_json = response.json()
+        self.refresh_token = response_json.get('refresh_token')
+        return response_json
 
     def authorization_url(self, redirect_uri: str, scope: str = None):
         """
@@ -67,7 +75,7 @@ class OAuthTools(object):
 
         return auth_url
 
-    def code_to_token_post_data(self, code: str, client_secret: str=None, redirect_uri: str=None):
+    def code_to_token_post_data(self, code: str, client_secret: str = None, redirect_uri: str = None):
         if redirect_uri:
             self.redirect_uri = redirect_uri
         elif not self.redirect_uri:
@@ -90,7 +98,7 @@ class OAuthTools(object):
 
         return form_data
 
-    def code_to_token(self, code: str, client_secret: str=None):
+    def code_to_token(self, code: str, client_secret: str = None):
         form_data = self.code_to_token_post_data(code, client_secret)
 
         return self._post_for_token(form_data)
@@ -101,17 +109,30 @@ class OAuthTools(object):
             'client_secret': self.client_secret,
             'grant_type': 'client_credentials'
         }
-        # print("doing client credential authentication ... ")
+        logging.debug("doing client credential authentication ... ")
         return self._post_for_token(params)
 
-    def password_grant(self, username: str, password: str, scope: str='openid'):
+    def password_grant(self, username: str, password: str, scope: str = None):
         params = {
             'client_id': self.client_id,
             'client_secret': self.client_secret,
             'grant_type': 'password',
             'username': username,
             'password': password,
-            'scope': scope
+            'scope': scope if scope is not None else self.scope
         }
-        # print("doing password based authentication with scope '%s' ... " % params['scope'])
+        logging.debug("doing password based authentication with scope '%s' ... " % params['scope'])
+        return self._post_for_token(params)
+
+    def token_refresh(self, scope: str = None):
+        if jwt_helper.get_claim(self.refresh_token, "exp") < time.time():
+            raise jwt.exceptions.ExpiredSignatureError()
+        params = {
+            'grant_type': 'refresh_token',
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'refresh_token': self.refresh_token,
+            'scope': scope if scope is not None else self.scope
+        }
+        logging.debug("doing token refresh with scope '%s' ... " % params['scope'])
         return self._post_for_token(params)
